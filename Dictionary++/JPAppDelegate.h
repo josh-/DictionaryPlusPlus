1d0a5e2b45ebdd8b867cb93949475018bd2433f2
Josh Parnham <joshparnham@gmail.com>
2013-04-01 10:28:47 +1100

    Initial Commit

---
+++ Dictionary++/JPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JPAppDelegate.h
+//  Dictionary++
+//
+//  Created by Josh Parnham on 1/04/13.
+//  Copyright (c) 2013 Josh Parnham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dictionary++/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dictionary++
+//
+//  Created by Josh Parnham on 1/04/13.
+//  Copyright (c) 2013 Josh Parnham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JPAppDelegate class]));
+    }
+}
